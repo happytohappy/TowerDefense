26e264880f391a61674c07f1b73eda39dfd6e563
tomodatsi79 <tomodatsi79@gmail.com>
2024-01-16 22:16:54 +0900

    코드 에러 수정

--- Assets/Scripts/Managers/Table/TableCharacter.cs
+++ Assets/Scripts/Managers/Table/TableCharacter.cs
@@ -1,41 +1,41 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public partial class TableManager : MonoBehaviour
+public partial class TableManager
 {
-    private Dictionary<int, CharacterData> m_dicCharacetrData = new Dictionary<int, CharacterData>();
+    //private Dictionary<int, CharacterData> m_dicCharacetrData = new Dictionary<int, CharacterData>();
 
-    private void InitCharacterTable()
-    {
-        TextAsset TextFile = Resources.Load<TextAsset>("Table/TableCharacter");
-        string CSVText = TextFile.text;
-        List<string> valueArray = Utility.CSVSplitData(CSVText);
-        for (int i = 2; i < valueArray.Count; i++)
-        {
-            string[] words = valueArray[i].Split(',');
-            CharacterData CharData = new CharacterData();
-            CharData.CharID = int.Parse(words[0]);
-            CharData.CharModel = Resources.Load(string.Format("Character/{0}", words[1])) as GameObject;
-            CharData.Name = words[2];
-            CharData.Grade = int.Parse(words[3]);
-            CharData.NameString = int.Parse(words[4]);
-            CharData.AttackType = int.Parse(words[5]);
-            CharData.Damage = int.Parse(words[6]);
-            CharData.Speed = int.Parse(words[7]);
-            CharData.CR = int.Parse(words[8]);
-            CharData.CRRate = int.Parse(words[9]);
-            CharData.DefaultSkill_1 = int.Parse(words[10]);
-            CharData.DefaultSkill_1 = int.Parse(words[11]);
+    //private void InitCharacterTable()
+    //{
+    //    TextAsset TextFile = Resources.Load<TextAsset>("Table/TableCharacter");
+    //    string CSVText = TextFile.text;
+    //    List<string> valueArray = Utility.CSVSplitData(CSVText);
+    //    for (int i = 2; i < valueArray.Count; i++)
+    //    {
+    //        string[] words = valueArray[i].Split(',');
+    //        CharacterData CharData = new CharacterData();
+    //        CharData.CharID = int.Parse(words[0]);
+    //        CharData.CharModel = Resources.Load(string.Format("Character/{0}", words[1])) as GameObject;
+    //        CharData.Name = words[2];
+    //        CharData.Grade = int.Parse(words[3]);
+    //        CharData.NameString = int.Parse(words[4]);
+    //        CharData.AttackType = int.Parse(words[5]);
+    //        CharData.Damage = int.Parse(words[6]);
+    //        CharData.Speed = int.Parse(words[7]);
+    //        CharData.CR = int.Parse(words[8]);
+    //        CharData.CRRate = int.Parse(words[9]);
+    //        CharData.DefaultSkill_1 = int.Parse(words[10]);
+    //        CharData.DefaultSkill_1 = int.Parse(words[11]);
 
-            m_dicCharacetrData.Add(CharData.CharID, CharData);
-        }
-    }
+    //        m_dicCharacetrData.Add(CharData.CharID, CharData);
+    //    }
+    //}
 
-    public CharacterData GetCharData(int CharID)
-    {
-        if (m_dicCharacetrData.ContainsKey(CharID))
-            return m_dicCharacetrData[CharID];
-        else
-            return null;
-    }
+    //public CharacterData GetCharData(int CharID)
+    //{
+    //    if (m_dicCharacetrData.ContainsKey(CharID))
+    //        return m_dicCharacetrData[CharID];
+    //    else
+    //        return null;
+    //}
 }
--- Assets/Scripts/Managers/Table/TableManager.cs
+++ Assets/Scripts/Managers/Table/TableManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TableManager : MonoBehaviour
+public partial class TableManager : MonoBehaviour
 {
     public void Init()
     {
