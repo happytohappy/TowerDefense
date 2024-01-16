c6ec4a956bf0aeb25ccca16f4f0f3fe6aab30709
youjin <youjin8932@gmail.com>
2024-01-16 21:40:09 +0900

    길찾기

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -21,6 +21,7 @@ public class Managers : MonoBehaviour
     [SerializeField] private Camera m_WorldCam = null;
     [SerializeField] private Camera m_UICam = null;
     [SerializeField] private UIWindowManager m_UI = null;
+    [SerializeField] private TableManager m_Table = null;
     [SerializeField] private SoundManager m_Sound = null;
     [SerializeField] private PoolManager m_Pool = null;
     [SerializeField] private ResourceManager m_Resource = null;
@@ -30,6 +31,7 @@ public class Managers : MonoBehaviour
     public static Camera WorldCam => GetInstance.m_WorldCam;
     public static Camera UICam => GetInstance.m_UICam;
     public static UIWindowManager UI => GetInstance.m_UI;
+    public static TableManager Table => GetInstance.m_Table;
     public static SoundManager Sound => GetInstance.m_Sound;
     public static PoolManager Pool => GetInstance.m_Pool;
     public static ResourceManager Resource => GetInstance.m_Resource;
