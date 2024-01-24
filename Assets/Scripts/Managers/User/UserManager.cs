4d8911cff370d0b305fc2d665ce9388ed82fd7ed
youjin <youjin8932@gmail.com>
2024-01-24 22:54:32 +0900

    User Manager 추가

--- Assets/Scripts/Managers/User/UserManager.cs
+++ Assets/Scripts/Managers/User/UserManager.cs
@@ -34,4 +34,33 @@ public class UserManager : MonoBehaviour
         else
             return null;
     }
+
+    /*
+     * public static void SaveLocalData<T>(T SaveData, LocalKey Key)
+    {
+        var binaryFormatter = new BinaryFormatter();
+        var memoryStream = new MemoryStream();
+
+        binaryFormatter.Serialize(memoryStream, SaveData);
+        PlayerPrefs.SetString(Key.ToString(), Convert.ToBase64String(memoryStream.GetBuffer()));
+    }
+
+    public static T LoadLocalData<T>(LocalKey key)
+    {
+        var data = PlayerPrefs.GetString(key.ToString());
+        
+        var binaryFormatter = new BinaryFormatter();
+        var memoryStream = new MemoryStream(Convert.FromBase64String(data));
+
+        // 가져온 데이터를 바이트 배열로 변환하고 사용하기 위해 다시 리스트로 캐스팅 해줍니다.
+        return (T)binaryFormatter.Deserialize(memoryStream);
+    }
+
+     if (PlayerPrefs.HasKey(LocalKey.Option.ToString()) == true)
+        {
+            Account.option = Utility.LoadLocalData<Option>(LocalKey.Option);
+            Utility.SaveLocalData<Option>(Account.option, LocalKey.Option);
+        }
+    */
+
 }
