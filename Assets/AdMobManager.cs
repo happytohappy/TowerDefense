bc071a3522f00e97cff29af0eb3f129fb677d17e
youjin <youjin8932@gmail.com>
2024-02-27 22:00:12 +0900

    몬스터 정리

---
+++ Assets/AdMobManager.cs
@@ -0,0 +1,56 @@
+using GoogleMobileAds.Api;
+using System;
+using UnityEngine;
+
+public class AdMobManager : MonoBehaviour
+{
+    private RewardedAd rewardedAd;
+    private int gold;
+
+    private void Start()
+    {
+        InitAds();
+    }
+
+    private void InitAds()
+    {
+        string adUnitId;
+
+#if UNITY_ANDROID
+        adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#else
+        adUnitId = "unexpected_platform";
+#endif
+
+        RewardedAd.Load(adUnitId, new AdRequest.Builder().Build(), LoadCallback);
+    }
+
+    private void LoadCallback(RewardedAd rewardedAd, LoadAdError loadAdError)
+    {
+        if (rewardedAd != null)
+        {
+            this.rewardedAd = rewardedAd;
+
+            Debug.Log("로드 성공");
+        }
+        else
+            Debug.Log(loadAdError.GetMessage());
+    }
+
+    public void ShowAds()
+    {
+        if (rewardedAd.CanShowAd())
+            rewardedAd.Show(GetReward);
+        else
+            Debug.Log("광고 재생 실패");
+    }
+
+    private void GetReward(Reward reward)
+    {
+        gold += (int)reward.Amount;
+
+        Debug.Log("골드 : " + gold.ToString());
+
+        InitAds();
+    }
+}
--- Assets/Scripts/Managers/Backend/BackendManager.cs
+++ Assets/Scripts/Managers/Backend/BackendManager.cs
@@ -1,4 +1,5 @@
 using BackEnd;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BackendManager : MonoBehaviour
@@ -9,8 +10,64 @@ public class BackendManager : MonoBehaviour
         var bro = Backend.Initialize(true);
 
         if (bro.IsSuccess())
+        {
+            //GetProbabilitysTest();
+
             Debug.Log("초기화 성공 : " + bro);
+        }
         else
             Debug.LogError("초기화 실패 : " + bro);
+    }
+
+    public class ProbabilityItem
+    {
+        public string itemID;
+        public string itemName;
+        public string hpPower;
+        public string percent;
+        public int num;
+        public override string ToString()
+        {
+            return $"itemID : {itemID}\n" +
+            $"itemName : {itemName}\n" +
+            $"hpPower : {hpPower}\n" +
+            $"percent : {percent}\n" +
+            $"num : {num}\n";
+        }
+    }
+
+    public void GetProbabilitysTest()
+    {
+        string selectedProbabilityFileId = "9635";
+
+        var bro = Backend.Probability.GetProbabilitys(selectedProbabilityFileId, 10); // 10연차;
+
+        if (!bro.IsSuccess())
+        {
+            Debug.LogError(bro.ToString());
+            return;
+        }
+
+        LitJson.JsonData json = bro.GetFlattenJSON()["elements"];
+
+        List<ProbabilityItem> itemList = new List<ProbabilityItem>();
+
+        for (int i = 0; i < json.Count; i++)
+        {
+            ProbabilityItem item = new ProbabilityItem();
+
+            item.itemID = json[i]["itemID"].ToString();
+            item.itemName = json[i]["itemName"].ToString();
+            item.hpPower = json[i]["hpPower"].ToString();
+            item.percent = json[i]["percent"].ToString();
+            item.num = int.Parse(json[i]["num"].ToString());
+
+            itemList.Add(item);
+        }
+
+        foreach (var item in itemList)
+        {
+            Debug.Log(item.ToString());
+        }
     }
 }
