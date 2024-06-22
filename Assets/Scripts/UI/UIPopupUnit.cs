1d416193442c6e260c04d2460bfd60afb9f8919c
happy2happygames <happy2happy.games@gmail.com>
2024-06-22 14:18:12 +0900

    유닛 팝업 수정

--- Assets/Scripts/UI/UIPopupUnit.cs
+++ Assets/Scripts/UI/UIPopupUnit.cs
@@ -1,32 +1,17 @@
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using System.Linq;
-using System.Collections.Generic;
+using TMPro;
 
 public class UIPopupUnit : UIWindowBase
 {
-    [Header("유닛 상세 정보")]
-    [SerializeField] private TMP_Text m_text_name = null;
-    [SerializeField] private Image m_Image_hero = null;
-    [SerializeField] private Image m_Image_lock = null;
-    [SerializeField] private Image m_img_unit_type = null;
-    [SerializeField] private List<Image> m_list_grade_star = new List<Image>();
-    [SerializeField] private TMP_Text m_text_level = null;
-    [SerializeField] private Image m_Image_equipment = null;
-    [SerializeField] private TMP_Text m_text_damage = null;
-    [SerializeField] private TMP_Text m_text_speed = null;
-    [SerializeField] private TMP_Text m_text_range = null;
-    [SerializeField] private List<GameObject> m_list_bg = new List<GameObject>();
+    [Header("영웅 정보")]
+    [SerializeField] private HeroBaseInfo m_hero_base_info = null;
+
+    [Header("버튼")]
     [SerializeField] private ExtentionButton m_btn_remove = null;
     [SerializeField] private ExtentionButton m_btn_merge = null;
     [SerializeField] private TMP_Text m_text_remove_tier = null;
     [SerializeField] private TMP_Text m_text_remove_energy = null;
-
-    [Header("유닛 스킬")]
-    [SerializeField] private List<Slot_Skill> m_skill = new List<Slot_Skill>();
-
-    public int ToolTipIndex { get; set; }
 
     private int m_kind;
 
@@ -50,16 +35,8 @@ public class UIPopupUnit : UIWindowBase
             Managers.UI.CloseLast();
 
         m_kind = param.m_kind;
-        ToolTipIndex = -1;
 
         RefreshUI();
-    }
-
-    public void OnClickClose()
-    {
-        GameController.GetInstance.InputInit();
-
-        Managers.UI.CloseLast();
     }
 
     public void RefreshUI()
@@ -69,40 +46,20 @@ public class UIPopupUnit : UIWindowBase
         m_btn_merge.Ex_SetActive(SameHero.Count >= 2);
 
         // 첫번째 타워 보여주기
-        SetUnitInfo();
+        SetHeroInfo();
     }
 
-    public void SetUnitInfo()
+    public void SetHeroInfo()
     {
-        // 내가 보유한 타워의 레벨과 등급을 불러와서
-        var hero = Managers.User.GetUserHeroInfo(m_kind);
-        if (hero == null)
+        // 영웅 정보 셋팅
+        m_hero_base_info.SetData(m_kind);
+
+        var heroTableInfo = Managers.Table.GetHeroInfoData(m_kind);
+        if (heroTableInfo == null)
             return;
 
-        var heroInfo = Managers.Table.GetHeroInfoData(m_kind);
-        if (heroInfo == null)
-            return;
-
-        Util.SetHeroName(m_text_name, m_kind);
-        Util.SetHeroImage(m_Image_hero, m_kind);
-        m_img_unit_type.Ex_SetImage(Util.GetUnitType(m_kind));
-
-        for (int i = 0; i < m_list_bg.Count; i++)
-            m_list_bg[i].Ex_SetActive(i == (int)heroInfo.m_rarity - 1);
-
-        m_Image_hero.Ex_SetColor(Color.white);
-        m_Image_lock.Ex_SetActive(false);
-        m_text_level.Ex_SetText($"Lv.{hero.m_level}");
-        m_Image_equipment.Ex_SetActive(true);
-        Util.SetGradeStar(m_list_grade_star, hero.m_grade);
-
-        var heroLevelInfo = Managers.Table.GetHeroLevelData(m_kind, hero.m_level);
-        m_text_damage.Ex_SetText(heroLevelInfo.m_atk.ToString());
-        m_text_speed.Ex_SetText(heroLevelInfo.m_speed.ToString());
-        m_text_range.Ex_SetText(heroLevelInfo.m_range.ToString());
-
-        m_text_remove_tier.Ex_SetText($"Tier {heroInfo.m_tier}");
-        switch (heroInfo.m_tier)
+        m_text_remove_tier.Ex_SetText($"Tier {heroTableInfo.m_tier}");
+        switch (heroTableInfo.m_tier)
         {
             case 1: m_text_remove_energy.Ex_SetText($"{CONST.STAGE_ENERGY_SELL_1}"); break;
             case 2: m_text_remove_energy.Ex_SetText($"{CONST.STAGE_ENERGY_SELL_2}"); break;
@@ -113,8 +70,6 @@ public class UIPopupUnit : UIWindowBase
             case 7: m_text_remove_energy.Ex_SetText($"{CONST.STAGE_ENERGY_SELL_7}"); break;
             case 8: m_text_remove_energy.Ex_SetText($"{CONST.STAGE_ENERGY_SELL_8}"); break;
         }
-
-        Util.SetSkill(m_skill, m_kind);
     }
 
     public void OnClickUnitRemove()
@@ -139,10 +94,5 @@ public class UIPopupUnit : UIWindowBase
         gui.OnCheckHeroSynergy();
 
         OnClickClose();
-    }
-
-    public void OnClickToolTip(int in_skill_index)
-    {
-        Util.OpenToolTip(m_skill[in_skill_index].Contents, m_skill[in_skill_index].GetRoot);
     }
 }
